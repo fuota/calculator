dfe5a6ad5f4b31e6c41e2702a9e7317d192daf9d
Tam Do <phuongtam1072@gmail.com>
2022-06-09 18:03:33 +0700

    Add clear and delete function

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const numberButtons=document.querySelectorAll(".number");
 const current=document.querySelector("#current");
 const previous=document.querySelector("#previous");
 const dotButton=document.getElementById("dot")
+const clearButton=document.getElementById("clear-button");
+const backspace=document.getElementById("backspace")
 
 
 numberButtons.forEach((btn) => {btn.addEventListener("click", ()=>{
@@ -16,3 +18,32 @@ dotButton.addEventListener("click", ()=> {
     if (current.textContent.includes(".")) return
     current.textContent += ".";
 })
+
+clearButton.addEventListener("click", ()=> {
+    current.textContent = "0";
+    previous.textContent="";
+    operation=undefined;
+    firstNum=undefined;
+    secondNum=undefined;
+    isFinished=false;
+})
+backspace.addEventListener("click", ()=>{
+    if (current.textContent!=="") {
+    current.textContent=current.textContent.slice(0,-1);
+    isFinished=false;
+    }
+})
+
+clearButton.addEventListener("click", ()=> {
+    current.textContent = "0";
+    previous.textContent="";
+    operation=undefined;
+    firstNum=undefined;
+    secondNum=undefined;
+})
+backspace.addEventListener("click", ()=>{
+    if (current.textContent!=="") {
+    current.textContent=current.textContent.slice(0,-1);
+    }
+})
+
